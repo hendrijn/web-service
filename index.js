0a4f1382e724c09fd6ca6c60746d3314ebb90493
hendrijn <hendrijn@dukes.jmu.edu>
2021-10-25 16:09:50 +0000

    Merged subfolder.

--- index.js
+++ index.js
@@ -1,109 +1,65 @@
-const { response } = require('express');
 const express = require('express');
 const service = express();
-service.use(express.json());
+const fs = require('fs');
+const mysql = require('mysql');
 
-//allows localhost access
+const json = fs.readFileSync('credentials.json', 'utf-8');
+const credentials = JSON.parse(json);
+
+//cross-origin resource sharing using custom middleware and an options wildcard endpoint.
 service.use((request, response, next) => {
-    response.set('Access-Control-Allow-Origin', '*');
-    next();
+  response.set('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 service.options('*', (request, response) => {
-    response.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-    response.sendStatus(200);
+  response.set('Access-Control-Allow-Headers', 'Content-Type');
+  response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  response.sendStatus(200);
 });
+//end resource sharing
 
+//create database connection
+const connection = mysql.createConnection(credentials);
+connection.connect(error => {
+	if(error) {
+		console.error(error);
+		process.exit(1);
+	}
+});
+//end database connection
+
+function rowToObject(row) {
+	return {
+		id: row.id,
+		name: row.name,
+		items: row.items,
+		total: row.total
+	};
+}
+
+//*********** HTTP Methods ***********\\
+service.get('/orders', (request, response) => {
+	
+	const selectQuery = 'SELECT * FROM orders';
+	connection.query(selectQuery, (error, rows) => {
+	if (error) {
+		response.status(500);
+		response.json({
+			ok: false,
+			results: error.message,
+		});
+	} else {
+		const orders = rows.map(rowToObject);
+		response.json({
+			ok: true,
+			results: orders
+		});
+	}
+	});
+});
+//create the port to listen
 const port = 5001;
 service.listen(port, () => {
-    console.log(`We're live on port ${port}!`);
-});
-
-const orders = {};
-let orderNumber = 0;
-
-//gets all orders
-service.get('/orders', (request, response) => {
-    response.json({
-        ok: true,
-        results: orders
-    })
-});
-
-//gets an order by number
-service.get('/orders/:custName', (request, response) => {
-    const custName = request.params.custName;
-    if (!orders.hasOwnProperty(custName)) {
-        response.status(400);
-        response.json({
-            ok: false,
-            results: `${custName} does not exist.`
-        });
-    } else {
-        response.json({
-            ok: true,
-            name: custName,
-            results: orders[custName]
-        });
-    }
-});
-
-//adds an order
-service.post('/orders/:custName', (request, response) => {
-    const custName = request.params.custName;
-
-    const orderInfo = {
-        "orderNumber": orderNumber++,
-        "items": request.body.items,
-        "total": request.body.total
-    }
-    orders[custName] = orderInfo;
-
-    response.json({
-        ok: true,
-        results: {
-            name: custName,
-            order: orders[custName]
-        }
-    })
-});
-
-//delets an order
-service.delete('/orders/:custName', (request, response) => {
-    const custName = request.params.custName;
-
-    if (!orders.hasOwnProperty(custName)) {
-        response.status(400);
-        response.json({
-            ok: false,
-            results: `There is no order for ${custName}.`
-        });
-    } else {
-        delete orders[custName];
-        response.json({
-            ok: true,
-        });
-    }
-});
-
-//edit customer name for order
-service.patch('/orders/:custName', (request, response) => {
-    const custName = request.params.custName;
-
-    if (!orders.hasOwnProperty(custName)) {
-        response.status(400);
-        response.json({
-            ok: false,
-            results: `There is no order for ${custName}.`
-        });
-    } else {
-        orders[request.body.name] = orders[custName];
-        delete orders[custName];
-        response.json({
-            ok: true,
-            results: orders[request.body.name]
-        });
-    }
-
+	console.log(`We're live in port ${port}!`);
 });
